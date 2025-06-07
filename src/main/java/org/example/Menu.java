61c8f882a079fef843fc491dab997cdf8cba9016
Dwalwooody <arseny.vasilko@gmail.com>
2025-06-07 13:54:30 +0100

    Added menu functionality to the app

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,9 +1,47 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Menu {
-    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+
         while (true) {
-            switch ()
+            System.out.println("1 - Add review");
+            System.out.println("2 - List All reviews");
+            System.out.println("3 - Delete review");
+            System.out.println("4 - Get reviews based on Date");
+            System.out.println("5 - Sort reviews by date");
+            System.out.println("6 or ... - exit");
+
+            String command = scanner.nextLine();
+            switch (command) {
+                case "1" -> {
+
+                }
+                case "2" -> {
+//                    Сервис получить все - Ларик. Итеративно sout все отзывы из дб,
+//                    если отзывов нет - вывсети соответствующее сообщение
+                }
+                case "3" -> {
+//                    Сервис удаление - Олег. Надо попросить у юзера через scanner,
+//                    принять, проверить что отзыв с таким ID существует и если да,
+//                    удалить, вывсети подтверждение
+
+                }
+                case "4" -> {
+//                    Сервис Получить по дате - Саша.
+//                    Принять дату, перевести в тип данных , если есть отзывы с этой даты,
+//                    получить и вывести все, если не вывести соответствующее уведомление
+                }
+                case "5" -> {
+
+                }
+                default -> {
+                    return;
+                }
+            }
         }
     }
 }
